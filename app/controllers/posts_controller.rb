fc7b5b60e00226c0889256fbfdbcb7743ab6ac6e
Alvin Andal <alvin.009.andal@gmail.com>
2015-09-24 21:38:16 -0700

    Checkpoint 35 completed, CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,26 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def edit

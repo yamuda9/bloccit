aa777a3a9ff65641199d886fd070d191488e103d
Alvin Andal <alvin.009.andal@gmail.com>
2015-09-28 00:06:15 -0700

    Assignment 36 completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150920064509) do
+ActiveRecord::Schema.define(version: 20150928054234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "copy"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string   "body"
@@ -29,6 +37,14 @@ ActiveRecord::Schema.define(version: 20150920064509) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,17 @@ posts = Post.all
   )
 end
 
+# Create Questions
+50.times do
+  Question.create!(
+    title: RandomData.random_sentence,
+    body:  RandomData.random_paragraph
+  )
+end
+
+questions = Question.all
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"

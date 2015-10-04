63816adbc2e3ca4b6b64a66b1eec4f77691945e0
Alvin Andal <alvin.009.andal@gmail.com>
2015-10-03 22:13:18 -0700

    Assignment 38 completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+  validates_length_of :name, minimum: 5
+  validates_length_of :description, minimum: 15
+  validates_presence_of :name, :description
 end

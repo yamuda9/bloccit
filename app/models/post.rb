7859cac9ac0c33c2b1ebc2cd577982140bf98392
Alvin Andal <alvin.009.andal@gmail.com>
2015-10-03 21:58:10 -0700

    Checkpoint 38 completed, validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,15 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
   belongs_to :topic
   has_many :comments, dependent: :destroy
+
+  validates_length_of :title, minimum: 5
+  validates_length_of :body, minimum: 20
+  validates_presence_of :title, :body, :topic
+
+=begin
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minumum: 20 }, presence: true
+  validates :topic, presence: true
+=end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,6 +8,16 @@ RSpec.describe Post, type: :model do
 
   it { should belong_to(:topic) }
 
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:topic) }
+
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(20) }
+
+#  it { should validate_length_of(:title, minumum: 5) }
+#  it { should validate_length_of(:body, minumum: 20) }
+
   describe "attributes" do
     it "should respond to title" do
       expect(post).to respond_to(:title)

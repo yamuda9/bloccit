a9d86cd2e17f2aa7c88687db9ec97a2ae8ae739b
Alvin Andal <alvin.009.andal@gmail.com>
2015-10-04 19:57:42 -0700

    Checkpoint 43 completed, posts and users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,14 @@
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
   belongs_to :topic
+  belongs_to :user
   has_many :comments, dependent: :destroy
+
+  default_scope { order('created_at DESC') }
 
   validates_length_of :title, minimum: 5
   validates_length_of :body, minimum: 20
-  validates_presence_of :title, :body, :topic
+  validates_presence_of :title, :body, :topic, :user
 
 =begin
   validates :title, length: { minimum: 5 }, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151004051913) do
+ActiveRecord::Schema.define(version: 20151005021131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,9 +40,11 @@ ActiveRecord::Schema.define(version: 20151004051913) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
+    t.integer  "user_id"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id", using: :btree
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"

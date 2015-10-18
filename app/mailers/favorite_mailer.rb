40fa4bb1fdf2a5bb780871a070b0df729e9e6c73
Alvin Andal <alvin.009.andal@gmail.com>
2015-10-18 15:47:32 -0700

    Assignment 44, favorites, completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -12,4 +12,15 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, subject: "New comment on #{post.title}")
   end
+
+  def new_post(user, post)
+    headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+    @user = user
+    @post = post
+
+    mail(to: post.user.email, subject: "New post, #{post.title}, has been favorited.")
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,7 @@ class Post < ActiveRecord::Base
   has_many :favorites, dependent: :destroy
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
+  after_create :favorite_post
 
   default_scope { order('rank DESC') }
 
@@ -36,5 +37,10 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  def favorite_post
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
   end
 end

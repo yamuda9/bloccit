aeede178564e7b08bf8259735449e597399f7a77
Alvin Andal <alvin.009.andal@gmail.com>
2015-10-18 20:04:29 -0700

    Checkpoint 46 completed, private topics

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,7 +3,7 @@ include RandomData
 
 RSpec.describe Topic, type: :model do
   let(:topic) { create(:topic) }
-  
+
   it { should have_many(:posts) }
   it { should have_many(:labelings) }
   it { should have_many(:labels).through(:labelings) }
@@ -23,6 +23,24 @@ RSpec.describe Topic, type: :model do
 
     it "should be public by default" do
       expect(topic.public).to be(true)
+    end
+  end
+
+  describe "scopes" do
+    before do
+      @public_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+      @private_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph, public: false)
+    end
+
+    describe "visible_to(user)" do
+      it "returns all topics if the user is present" do
+        user = User.new
+        expect(Topic.visible_to(user)).to eq(Topic.all)
+      end
+
+      it "returns onl public topics if user is nil" do
+        expect(Topic.visible_to(nil)).to eq([@public_topic])
+      end
     end
   end
 end

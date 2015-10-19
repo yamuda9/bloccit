b1e51868642c58880390b247aa1360853296007c
Alvin Andal <alvin.009.andal@gmail.com>
2015-10-18 20:48:18 -0700

    Assignment 46, private topics, completed

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -42,5 +42,17 @@ RSpec.describe Topic, type: :model do
         expect(Topic.visible_to(nil)).to eq([@public_topic])
       end
     end
+
+    describe "publicly_viewable" do
+      it "checks if public topics are publicly viewable" do
+        expect(Topic.publicly_viewable).to eq([@public_topic])
+      end
+    end
+
+    describe "privately_viewable" do
+      it "checks if private topics are privately viewable" do
+        expect(Topic.privately_viewable).to eq([@private_topic])
+      end
+    end
   end
 end

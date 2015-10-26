82b838017d02f7e450a477abed7bdc576fe29059
Alvin Andal <alvin.009.andal@gmail.com>
2015-10-25 18:29:04 -0700

    Checkpoint 48 completed, api retrieving

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016045623) do
+ActiveRecord::Schema.define(version: 20151025230448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(version: 20151016045623) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 include RandomData
 
-RSpec.describe User, type: :model do  
+RSpec.describe User, type: :model do
   let(:user) { create(:user) }
   it { should have_many(:posts) }
   it { should have_many(:comments) }
@@ -14,7 +14,7 @@ RSpec.describe User, type: :model do
 
   # Shoulda tests for email
   it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
   it { should validate_length_of(:email).is_at_least(3) }
   it { should allow_value("user@bloccit.com").for(:email) }
   it { should_not allow_value("userbloccit.com").for(:email) }
@@ -117,6 +117,12 @@ RSpec.describe User, type: :model do
     it "returns the proper Gravatar url for a known email entry" do
       expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
       expect(User.avatar_url(known_user, 48)).to eq(expected_gravatar)
+    end
+  end
+
+  describe "#generate_auth_token" do
+    it "creates a token" do
+      expect(user.auth_token).to_not be_nil
     end
   end
 end
